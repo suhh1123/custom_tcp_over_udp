6a7c38e729d700b0148e19391eb16be9b0cc8d6c
Haohua Su <hs3225@columbia.edu>
2021-11-24 20:26:30 -0500

    record client and server action in two logs

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import datetime
 import socket
 import sys
 import time
@@ -9,27 +10,27 @@ class TCPClient(BaseException):
     def __init__(self, sourceFile, udplIP, udplPort, windowSizeInByte, ackPort):
         self.buffer = []
         self.sending_time_cache = []
-        self.MSS = 128 # measured in byte, 1 character takes exactly 1 byte
+        self.MSS = 512 # measured in byte, 1 character takes exactly 1 byte
         self.headerSize = 20
-
         self.sourceFile = sourceFile
+        self.logFile = "client_log.txt"
         self.udplIP = udplIP
         self.udplPort = udplPort
         self.windowSizeInByte = windowSizeInByte
         self.windowSizeInCount = windowSizeInByte // self.MSS
         self.ackPort = ackPort
-
-        self.timeoutInterval = 0.1
-        self.estimatedRTT = 0.1
+        self.timeoutInterval = 1
+        self.estimatedRTT = 1
         self.devRTT = 0
 
     def initiateCommunication(self):
-        # initiate UDP sockets for sending segments and receiving ACKs
+
+        # TODO Initiate UDP sockets for sending segments and receiving ACKs
         sendSocket = socket(AF_INET, SOCK_DGRAM)
         ackSocket = socket(AF_INET, SOCK_DGRAM)
         ackSocket.bind(('localhost', self.ackPort))
 
-        # parse the source file to the TCP client side buffer
+        # TODO Parse the source file to the client side buffer
         try:
             file = open(self.sourceFile, 'r')
         except IOError:
@@ -39,44 +40,70 @@ class TCPClient(BaseException):
             sys.exit()
         self.readInBuffer(file)
 
-        # send segments with timeout mechanism (fixed timeout interval for now)
+        # TODO Open the client log
+        try:
+            log = open(self.logFile, 'w')
+        except IOError:
+            print("Client log does not exist")
+            sys.exit()
+
+        # TODO Send segments with timeout mechanism
         processor = segmentProcessor()
         largest_inorder_sequence_number = -1
         leftBound = 0
         rightBound = self.windowSizeInCount - 1
+
+        # Send all segments in the window in a row
         for i in range(leftBound, rightBound + 1):
             sendSocket.sendto(self.buffer[i], (self.udplIP, self.udplPort))
             self.sending_time_cache.append(time.time())
+            # Write sending log
+            send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, send_data = processor.disassemble_segment(self.buffer[i])
+            self.writeLog(log, "SEND", send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, self.timeoutInterval)
 
         while largest_inorder_sequence_number < len(self.buffer) - 1:
             try:
                 ackSocket.settimeout(self.timeoutInterval)
                 while largest_inorder_sequence_number < len(self.buffer) - 1:
                     ackSegment = ackSocket.recv(2048)
-                    sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum, data = processor.disassemble_segment(
-                        ackSegment)
-                    if ack == 1 and sequenceNumber == largest_inorder_sequence_number + 1:
+                    ack_sourcePort, ack_destPort, ack_sequenceNumber, ack_ackNumber, ack_headerLength, ack_ack, ack_fin, ack_windowSize, ack_checkSum, ack_data = processor.disassemble_segment(ackSegment)
+                    if ack_ack == 1 and ack_sequenceNumber == largest_inorder_sequence_number + 1:
                         largest_inorder_sequence_number += 1
                         leftBound += 1
                         rightBound += 1
-                        # update timeout interval, we know the client received an inorder segment, record the current time, and look for the time when the segment was sent in the time buffer
+
+                        # Update timeout interval, we know the client received an inorder segment, record the current
+                        # time, and look for the time when the segment was sent in the sending time cache
                         sendTime = self.sending_time_cache[largest_inorder_sequence_number]
                         ackTime = time.time()
                         sampleRTT = ackTime - sendTime
-                        self.estimatedRTT = (0.875) * self.estimatedRTT + (0.125) * sampleRTT
-                        self.devRTT = (0.75) * self.devRTT + (0.25) * abs(sampleRTT - self.estimatedRTT)
+                        self.estimatedRTT = 0.875 * self.estimatedRTT + 0.125 * sampleRTT
+                        self.devRTT = 0.75 * self.devRTT + 0.25 * abs(sampleRTT - self.estimatedRTT)
                         self.timeoutInterval = self.estimatedRTT + 4 * self.devRTT
+                        print("ack " + str(largest_inorder_sequence_number) + " received ")
+
+                        # Write receiving log
+                        self.writeLog(log, "RECEIVE", ack_sourcePort, ack_destPort, ack_sequenceNumber, ack_ackNumber, ack_headerLength, ack_ack, ack_fin, ack_windowSize, ack_checkSum, self.timeoutInterval)
 
                         if rightBound < len(self.buffer):
                             ackSocket.sendto(self.buffer[rightBound], (self.udplIP, self.udplPort))
                             self.sending_time_cache.append(time.time())
                             ackSocket.settimeout(self.timeoutInterval)
+
+                            # Write sending log
+                            send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, send_data = processor.disassemble_segment(self.buffer[i])
+                            self.writeLog(log, "SEND", send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, self.timeoutInterval)
             except timeout:
                 for i in range(leftBound, rightBound + 1):
                     if i < len(self.buffer):
                         sendSocket.sendto(self.buffer[i], (self.udplIP, self.udplPort))
+                        self.sending_time_cache[i] = time.time()
 
-        # close sockets
+                        # Write resending log
+                        send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, send_data = processor.disassemble_segment(self.buffer[i])
+                        self.writeLog(log, "RESEND", send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, self.timeoutInterval)
+
+        # TODO Close sockets
         sendSocket.close()
         ackSocket.close()
 
@@ -88,7 +115,7 @@ class TCPClient(BaseException):
         currentData = file.read(self.MSS)
         while len(currentData) > 0:
             nextData = file.read(self.MSS)
-            # if the next data read from the file is empty, that means the current data is the final segment
+            # If the next data read from the file is empty, that means the current data is the final segment
             if len(nextData) == 0:
                 segment = processor.assemble_segment(self.ackPort, self.udplPort, sequenceNumber, ackNumber, 0, 1, self.windowSizeInByte, currentData)
             else:
@@ -97,6 +124,17 @@ class TCPClient(BaseException):
             currentData = nextData
             sequenceNumber += 1
             ackNumber += 1
+
+    def writeLog(self, log, status, sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum, timeoutInterval):
+        content = "[" + status + " - " + "Time: " + str(datetime.datetime.now()) + " - source port: " + str(sourcePort) + \
+                  " - dest port: " + str(destPort) + " - sequence number: " + str(sequenceNumber) + \
+                  " - ack number: " + str(ackNumber) + " - header length: " + str(headerLength) + \
+                  " - ACK: " + str(ack) + " - FIN: " + str(fin) + " - window size: " + str(windowSize) + \
+                  " - checksum: " + str(checkSum)
+        if (status == "SEND" or status == "RESEND"):
+            content += " - timeout interval: " + str(timeoutInterval)
+        content += "]\n"
+        log.write(content)
 
 # test
 if __name__ == '__main__':
@@ -112,16 +150,8 @@ if __name__ == '__main__':
     sourceFile = "source_file.txt"
     udplIP = "localhost"
     udplPort = 41192
-    windowSizeInByte = 512
+    windowSizeInByte = 1536
     ackPort = 9000
 
     client = TCPClient(sourceFile, udplIP, udplPort, windowSizeInByte, ackPort)
     client.initiateCommunication()
-
-    processor = segmentProcessor()
-    for i in range(0, len(client.buffer)):
-        print(client.buffer[i])
-        sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum, data = processor.disassemble_segment(
-            client.buffer[i])
-        print(sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum, data)
-        print()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import codecs
 import socket
 import struct
 import sys
+import datetime
 from socket import *
 
 from segmentProcessor import segmentProcessor
@@ -10,17 +11,18 @@ from segmentProcessor import segmentProcessor
 class TCPServer:
     def __init__(self, destFile, listeningPort, ackIP, ackPort):
         self.destFile = destFile
+        self.logFile = "server_log.txt"
         self.listeningPort = listeningPort
         self.ackIP = ackIP
         self.ackPort = ackPort
 
     def initiateCommunication(self):
-        # initiate UDP sockets for receiving segments and sending ACKs
+        # TODO: Initiate UDP sockets for receiving segments and sending ACKs
         receiveSocket = socket(AF_INET, SOCK_DGRAM)
         receiveSocket.bind(('localhost', self.listeningPort))
         ackSocket = socket(AF_INET, SOCK_DGRAM)
 
-        # build tunnel to destination file
+        # TODO Open the destination file
         try:
             file = open(self.destFile, 'w')
         except IOError:
@@ -29,18 +31,27 @@ class TCPServer:
             ackSocket.close()
             sys.exit()
 
-        # receive segment and send ACK
+        # TODO Open the server log
+        try:
+            log = open(self.logFile, 'w')
+        except IOError:
+            print("Server log does not exist")
+            sys.exit()
+
+        # TODO Receive segment and send ACK
         processor = segmentProcessor()
         largest_inorder_sequence_number = -1
+        flag = True
 
-        while True:
+        while flag:
             segment, clientAddress = receiveSocket.recvfrom(2048)
 
             if segment:
-                # disassemble the received segment
+                # Disassemble the received segment
                 sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum, data = processor.disassemble_segment(segment)
 
-                # reversed engineer, calculating the checksum on the entire segment with checksum not equal to 0 somehow throws exception
+                # Reversed engineer, calculating the checksum on the entire segment with checksum not equal to 0
+                # somehow throws exception
                 headerLength = 20
                 flags = (ack << 4) + fin
                 urgPointer = 0
@@ -48,18 +59,36 @@ class TCPServer:
                 raw_segment = raw_header + codecs.encode(data, encoding="UTF-16")
                 decoded_msg = codecs.decode(raw_segment, encoding="UTF-16")
 
-                # check if segment has been corrupted
-                if (processor.calculateCheckSum(decoded_msg) == checkSum):
+                # Check if segment has been corrupted and received in order
+                if (processor.calculateCheckSum(decoded_msg) == checkSum and sequenceNumber == largest_inorder_sequence_number + 1):
+                    if fin:
+                        file.write(data.rstrip(' \t\r\n\0'))
+                        flag = False
+                    else:
+                        file.write(data)
 
-                    if (sequenceNumber == largest_inorder_sequence_number + 1):
-                        if fin:
-                            file.write(data.rstrip(' \t\r\n\0'))
-                        else:
-                            file.write(data)
+                    largest_inorder_sequence_number += 1
+                    ackSegment = processor.assemble_segment(self.listeningPort, self.ackPort, largest_inorder_sequence_number, largest_inorder_sequence_number + 1, 1, 0, windowSize, "")
+                    ackSocket.sendto(ackSegment, (self.ackIP, self.ackPort))
 
-                        largest_inorder_sequence_number += 1
-                        ackSegment = processor.assemble_segment(self.listeningPort, self.ackPort, largest_inorder_sequence_number, largest_inorder_sequence_number + 1, 1, 0, 0, "")
-                        ackSocket.sendto(ackSegment, (self.ackIP, self.ackPort))
+                    # Write receiving log
+                    self.writeLog(log, "RECEIVE", sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum)
+
+                    # Write sending log
+                    send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum, send_data = processor.disassemble_segment(ackSegment)
+                    self.writeLog(log, "SEND", send_sourcePort, send_destPort, send_sequenceNumber, send_ackNumber, send_headerLength, send_ack, send_fin, send_windowSize, send_checkSum)
+
+        # TODO Close sockets
+        receiveSocket.close()
+        ackSocket.close()
+
+    def writeLog(self, log, status, sourcePort, destPort, sequenceNumber, ackNumber, headerLength, ack, fin, windowSize, checkSum):
+        content = "[" + status + " - " + "Time: " + str(datetime.datetime.now()) + " - source port: " + str(sourcePort) + \
+                  " - dest port: " + str(destPort) + " - sequence number: " + str(sequenceNumber) + \
+                  " - ack number: " + str(ackNumber) + " - header length: " + str(headerLength) + \
+                  " - ACK: " + str(ack) + " - FIN: " + str(fin) + " - window size: " + str(windowSize) + \
+                  " - checksum: " + str(checkSum) + "]\n"
+        log.write(content)
 
 if __name__ == '__main__':
     # try:
